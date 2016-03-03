5fe983dcf3f677672fa7303a5d51038f50ed3188
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-03-03 15:02:52 -0800

    chore(build): replace all jekyll templates with jade

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,13 @@ var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
 var concat  = require("gulp-concat");
 var cssnano = require("gulp-cssnano");
+var data  = require("gulp-data");
 var gulp  = require("gulp");
 var gutil = require("gulp-util");
 var imagemin = require("gulp-imagemin");
 var jade = require("gulp-jade");
 var sass  = require("gulp-sass");
 var uglify = require("gulp-uglify");
-
-var bower_files = ["bower_components/jquery/dist/jquery.js"]
 
 // Build styles. Only uses main.scss
 gulp.task("build:styles", function () {
@@ -23,7 +22,6 @@ gulp.task("build:styles", function () {
     }))
     .pipe(cssnano())
     // for jekyll
-    .pipe(gulp.dest("css/"))
     // for browsersync
     .pipe(gulp.dest("_site/css/"))
     .pipe(browserSync.stream())
@@ -33,7 +31,6 @@ gulp.task("build:styles", function () {
 gulp.task("build:images", function () {
   return gulp.src(["_app/img/*"])
     .pipe(imagemin())
-    .pipe(gulp.dest("img/"))
     .pipe(gulp.dest("_site/img/"))
     .on("error", gutil.log);
 });
@@ -42,23 +39,34 @@ gulp.task("build:scripts", function () {
   return gulp.src(["_app/js/*"])
     .pipe(concat("main.js"))
     .pipe(uglify())
-    .pipe(gulp.dest("js/"))
     .pipe(gulp.dest("_site/js/"))
     .on("error", gutil.log);
 });
 
 gulp.task("build:jade", function () {
   return gulp.src(["_app/jade/**/*.jade","!_app/jade/**/_*.jade"])
+    .pipe(data(function(file) {
+      return require("./_config.json");
+    }))
     .pipe(jade())
     .pipe(gulp.dest("_site"))
     .on("error", gutil.log);
-})
+});
+
+gulp.task("build:styles:vendor", function () {
+  return gulp.src([
+    "bower_components/pure/pure-min.css",
+    "bower_components/pure/grids-responsive-min.css",
+    "bower_components/pure/grids-responsive-old-ie-min.css",
+  ])
+  .pipe(gulp.dest("_site/css/"));
+});
 
 gulp.task("bs:reload", function () {
   browserSync.reload();
 });
 
-gulp.task("build", ["build:scripts", "build:images", "build:styles", "build:jekyll"], function () {
+gulp.task("build", ["build:scripts", "build:images", "build:styles", "build:jade", "build:styles:vendor"], function () {
   // nop
 });
 
@@ -66,9 +74,9 @@ gulp.task("serve", ["build"], function () {
   browserSync.init({
     server: "./_site",
   });
-  gulp.watch("_config.yml",["build:jekyll"]);
+  gulp.watch("_config.json",["build:jade"]);
   gulp.watch("_app/css/**/*.scss",["build:styles"]);
   gulp.watch("_app/js/**/*.js",["build:scripts","bs:reload"]);
-  gulp.watch("_posts/**/*",["build:jekyll","bs:reload"]);
-  gulp.watch(["**/*.html","!_site/**/*"],["build:jekyll","bs:reload"]);
+  gulp.watch("_app/jade/**/*.jade",["build:jade","bs:reload"]);
+  gulp.watch(["**/*.html","!_site/**/*"],["build:jade","bs:reload"]);
 });
